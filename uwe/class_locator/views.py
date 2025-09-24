1a08b13bcaba03d46e61405f51ab0787c1d8043d
Okorie Dominion <156934036+Nick24d@users.noreply.github.com>
2025-09-24 13:21:50 +0000

    changes to view

--- uwe/class_locator/views.py
+++ uwe/class_locator/views.py
@@ -1,6 +1,6 @@
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
-from .models import Room
+from .models import Room, Block
 from .serializer import RoomSerializer
 
 @api_view(["GET"])
@@ -10,5 +10,26 @@ def lookup_room(request, code):
         serializer = RoomSerializer(room)
         return Response(serializer.data)
     except Room.DoesNotExist:
-        return Response({"error": "Room not found"}, status=404)
+        block_letter = "".join([c for c in code if c.isalpha()][:1])  # "A"
+        floor_digit = int(code[0]) if code[0].isdigit() else None
+
+        block = Block.objects.filter(name=block_letter).first()
+
+        if block:
+            return Response({
+                "room_code": code,
+                "exists": False,
+                "block": {
+                    "name": block.name,
+                    "description": block.description,
+                },
+                "floor": floor_digit,
+                "message": f"Room not in database. Estimated location: Block {block.name}, Floor {floor_digit}."
+            })
+        else:
+            return Response({
+                "room_code": code,
+                "exists": False,
+                "error": "Block not recognized"
+            }, status=404)
 
