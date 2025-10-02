e3f9b3a846287e581bc8384e6675514626cd189d
Okorie Dominion <156934036+Nick24d@users.noreply.github.com>
2025-10-02 08:07:24 +0000

    adding login

--- uwe/class_locator/models.py
+++ uwe/class_locator/models.py
@@ -1,4 +1,12 @@
 from django.db import models
+from django.contrib.auth.models import User
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    email_verified = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.user.username
 
 class Block(models.Model):
     name = models.CharField(max_length=6, unique=True)
--- uwe/class_locator/serializer.py
+++ uwe/class_locator/serializer.py
@@ -1,5 +1,6 @@
 from rest_framework import serializers
-from .models import Block, Room
+from .models import Block, Room, Profile
+from django.contrib.auth.models import User
 
 class BlockSerializer(serializers.ModelSerializer):
     class Meta:
@@ -11,3 +12,21 @@ class RoomSerializer(serializers.ModelSerializer):
     class Meta:
         model = Room
         fields = ["room_code", "block", "floor", "name", "coordinates"]
+
+class RegisterSerializer(serializers.ModelSerializer):
+    password = serializers.CharField(write_only=True)
+
+    class Meta:
+        model = User
+        fields = ["username", "email", "password"]
+
+    def create(self, validated_data):
+        user = User.objects.create_user(
+            username=validated_data["username"],
+            email=validated_data["email"],
+            password=validated_data["password"]
+            extra_kwargs={"password": {"write_only": True}}
+        )
+        user.is_active = False  # User must verify email
+        user.save()
+        return user
